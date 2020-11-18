42dec5d5fa732660d0ebefbdd9e5668b212d85f1
Samuel Holmes <sam@edge.app>
2020-11-18 13:56:53 -0800

    Cleanup unused types

--- src/types.ts
+++ src/types.ts
@@ -65,27 +65,6 @@ export const asStoreFileDocument = asObject({
   ...asStoreFile.shape
 })
 
-export interface DocumentRequest {
-  [Key: string]: number
-}
-
-export interface Results {
-  [Key: string]: StoreDocument
-}
-
-export interface StoreDocument {
-  timestamp: number
-  files?: object
-  content?: string
-}
-
-export type DbResponse = Nano.DocumentGetResponse & StoreDocument
-
-export interface File {
-  timestamp: number
-  content: string
-}
-
 export interface ApiResponse<Data> {
   success: true
   data: Data
