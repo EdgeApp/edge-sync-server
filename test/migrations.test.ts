8bdabbe2f5d6a5bd7f43b6822c0addfa46705cb9
Samuel Holmes <sam@edge.app>
2021-03-01 16:54:36 -0800

    Tests for migration in getUpdates endpoint

--- test/migrations.test.ts
+++ test/migrations.test.ts
@@ -3,11 +3,15 @@ import { it } from 'mocha'
 import supertest from 'supertest'
 
 import { AppState, makeServer } from '../src/server'
-import { GetFilesResponse, GetStoreResponse } from '../src/types'
+import {
+  GetFilesResponse,
+  GetStoreResponse,
+  GetUpdatesResponse
+} from '../src/types'
 import { apiSuite } from './suites'
 import { isSuccessfulResponse } from './utils'
 
-apiSuite('Migrations', (appState: AppState) => {
+apiSuite('Migrations (v3 getFiles)', (appState: AppState) => {
   const app = makeServer(appState)
   const agent = supertest.agent(app)
 
@@ -79,5 +83,59 @@ apiSuite('Migrations', (appState: AppState) => {
         `Expected 'timestamp' field to be a timestamp in response data`
       )
     }
+  })
+})
+
+apiSuite('Migrations (v3 getUpdates)', (appState: AppState) => {
+  const app = makeServer(appState)
+  const agent = supertest.agent(app)
+
+  const v2Hostnames = appState.config.migrationOriginServers.map(
+    url => new URL(url).hostname
+  )
+  const v2Agents = v2Hostnames.map(hostname =>
+    supertest.agent(`https://${hostname}`)
+  )
+
+  const repoId = appState.config.testMigrationRepo
+  const paths: string[] = []
+
+  // Fixtures:
+
+  before(async () => {
+    // Get the repo/store data from one of the V2 servers
+    const responses = await Promise.all(
+      v2Agents.map(v2Agent => v2Agent.get(`/api/v2/store/${repoId}`))
+    )
+
+    const successfulResponse = responses.find(res => res.status === 200)
+
+    if (successfulResponse == null) {
+      throw new Error(
+        'Unable to find testMigrationRepo in any of the migrationOriginServers'
+      )
+    }
+
+    Object.keys(successfulResponse.body.changes).forEach(key => {
+      // Add leading forward-slash
+      paths.push(`/${key}`)
+    })
+  })
+
+  // Tests:
+
+  it('GET /api/v3/getUpdates passive migration', async () => {
+    const res = await agent
+      .post('/api/v3/getUpdates')
+      .send({
+        repoId,
+        timestamp: 0
+      })
+      .expect(isSuccessfulResponse)
+
+    const data: GetUpdatesResponse = res.body.data
+
+    expect(Object.keys(data.paths)).to.have.members(paths)
+    expect(data.deleted).deep.equal({})
   })
 })
