8a471e19e5f9f010bbb4700298802d67629bf0bf
Samuel Holmes <sam@edge.app>
2021-03-03 10:25:38 -0800

    Cleanup config.schema.ts
    Remove unnecessary inline comments in the asConfig cleaner

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -3,26 +3,18 @@ import { asArray, asNumber, asObject, asOptional, asString } from 'cleaners'
 export type Config = ReturnType<typeof asConfig>
 
 export const asConfig = asObject({
-  // sync_datastore
   couchDatabase: asString,
-  // http://admin:{password}@localhost:5984
   couchUri: asString,
   couchPassword: asString,
-  // CouchDB sharding parmeters
   couchSharding: asObject({
-    // Number of shards (24)
     q: asNumber,
-    // Number of shard replicas (1)
     n: asNumber
   }),
-  httpPort: asNumber, // 8000
+  httpPort: asNumber,
   maxTimestampHistoryAge: asNumber,
-  maxPageSize: asNumber, // 100
-  // Number of processes to fork in the cluster (defaults to number of CPU cores)
+  maxPageSize: asNumber,
   instanceCount: asOptional(asNumber),
-  // URL of the servers from which to migrate repos.
   migrationOriginServers: asArray(asString),
-  // Temp directory to use for repo migrations
   migrationTmpDir: asString,
   testMigrationRepo: asString
 })
