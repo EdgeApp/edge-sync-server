21fb703c233a07509614cb30ad095462e74102f2
Samuel Holmes <sam@edge.app>
2021-04-05 18:18:59 -0700

    Simplified `yarn setup` script
    * Always non-interactive
    * Use ENV exclusively for customization
    * Production-first configuration for default config

--- setup.ts
+++ setup.ts
@@ -1,41 +1,9 @@
 import { writeFileSync } from 'fs'
-import minimist from 'minimist'
-import { createInterface } from 'readline'
-import { Writable } from 'stream'
 
-import { Config, configSample } from './src/config.schema'
-
-const argv = minimist(process.argv.slice(2))
-
-const isProduction = argv.dev !== true
-const { COUCH_HOSTNAME, COUCH_PASSWORD } = process.env
+import { Config, configTemplate } from './src/config.schema'
 
 async function main(): Promise<void> {
-  const couchHostname: string = COUCH_HOSTNAME ?? configSample.couchHostname
-  const couchPassword: string =
-    typeof COUCH_PASSWORD === 'string' && COUCH_PASSWORD !== ''
-      ? COUCH_PASSWORD
-      : await captureInput('Enter CouchDB password: ', true)
-
-  const config: Config = isProduction
-    ? {
-        ...configSample,
-        couchHostname,
-        couchPassword,
-        couchSharding: {
-          q: 64,
-          n: 2
-        },
-        migrationOriginServers: [
-          'https://git-uk.edge.app/repos/',
-          'https://git3.airbitz.co/repos/',
-          'https://git-eusa.edge.app/repos/'
-        ],
-        migrationTmpDir: '/tmp/apps/edge-sync-server/'
-      }
-    : { ...configSample, couchPassword }
-
-  if (isProduction) delete config.instanceCount
+  const config: Config = configTemplate
 
   writeFileSync('config.json', JSON.stringify(config, null, 2), 'utf8')
 }
@@ -43,27 +11,3 @@ async function main(): Promise<void> {
 main().catch(err => {
   throw err
 })
-
-function captureInput(prompt: string, muted: boolean = false): Promise<string> {
-  let isMuted: boolean = false
-  const mutableStdout = new Writable({
-    write: function (chunk, encoding, callback) {
-      if (!isMuted) process.stdout.write(chunk, encoding)
-      callback()
-    }
-  })
-
-  const rl = createInterface({
-    input: process.stdin,
-    output: mutableStdout,
-    terminal: true
-  })
-
-  return new Promise((resolve, reject) => {
-    rl.question(prompt, val => {
-      resolve(val)
-      rl.close()
-    })
-    isMuted = muted
-  })
-}
--- src/config.schema.ts
+++ src/config.schema.ts
@@ -2,6 +2,8 @@ import { asArray, asNumber, asObject, asOptional, asString } from 'cleaners'
 
 export type Config = ReturnType<typeof asConfig>
 
+// Config:
+
 export const asConfig = asObject({
   couchDatabase: asString,
   couchUri: asString,
@@ -20,19 +22,31 @@ export const asConfig = asObject({
   testMigrationRepo: asString
 })
 
-export const configSample: Config = {
+// Customization:
+
+const {
+  NODE_ENV = 'production',
+  COUCH_HOSTNAME = 'localhost',
+  COUCH_PASSWORD = 'password'
+} = process.env
+
+const isDev = NODE_ENV === 'dev'
+
+// Default:
+
+export const configTemplate: Config = {
   couchDatabase: 'sync_datastore',
   couchUri: 'http://admin:{password}@{hostname}:5984',
-  couchHostname: 'localhost',
-  couchPassword: 'password123',
+  couchHostname: COUCH_HOSTNAME,
+  couchPassword: COUCH_PASSWORD,
   couchSharding: {
-    q: 24,
-    n: 1
+    q: 16,
+    n: 3
   },
   httpPort: 8008,
   maxTimestampHistoryAge: 2592000000,
   maxPageSize: 100,
-  instanceCount: 4,
+  instanceCount: isDev ? 4 : undefined,
   migrationOriginServers: [
     'https://git-uk.edge.app/repos/',
     'https://git3.airbitz.co/repos/',
