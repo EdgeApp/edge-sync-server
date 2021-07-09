4ae1dab32a7553b09d969868ba73e5b6362f6b8c
Samuel Holmes <sam@edge.app>
2021-07-08 17:11:25 -0700

    Omit .ts file extension for stress-test worker-cluster to fix build

--- src/bin/stress-test/index.ts
+++ src/bin/stress-test/index.ts
@@ -140,7 +140,7 @@ async function main(): Promise<void> {
   // Worker Cluster Process
   // ---------------------------------------------------------------------
   // spawn
-  const workerCluster = fork(join(__dirname, 'worker-cluster.ts'))
+  const workerCluster = fork(join(__dirname, 'worker-cluster'))
   // events
   workerCluster.on('message', payload => {
     try {
