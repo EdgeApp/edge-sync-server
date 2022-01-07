c0d93b8b19979f4a98b6f4ef7f1d0c18b7ba087f
Samuel Holmes <sam@edge.app>
2022-01-06 17:47:14 -0800

    Add whitelistApiKeys middleware to PUT /store/:syncKey endpoint

--- src/v2/routes/putStore.ts
+++ src/v2/routes/putStore.ts
@@ -10,40 +10,45 @@ import { AppState } from '../../server'
 import { syncKeyToRepoId } from '../../util/security'
 import { ServerError } from '../../util/server-error'
 import { checkRepoExists, createRepoDocument } from '../../util/store/repo'
-import { whitelistIps } from '../../whitelisting'
+import { whitelistApiKeys, whitelistIps } from '../../whitelisting'
 
 export const putStoreRouter = (appState: AppState): Router => {
   const router = PromiseRouter()
 
-  router.put('/store/:syncKey', whitelistIps(appState), async (req, res) => {
-    let params: PutStoreParams
+  router.put(
+    '/store/:syncKey',
+    whitelistIps(appState),
+    whitelistApiKeys(appState),
+    async (req, res) => {
+      let params: PutStoreParams
 
-    // Request body validation
-    try {
-      params = asPutStoreParams(req.params)
-    } catch (error) {
-      throw new ServerError(400, error.message)
+      // Request body validation
+      try {
+        params = asPutStoreParams(req.params)
+      } catch (error) {
+        throw new ServerError(400, error.message)
+      }
+
+      const { syncKey } = params
+      const repoId = syncKeyToRepoId(syncKey)
+
+      if (await checkRepoExists(appState)(repoId)) {
+        throw new ServerError(409, 'Datastore already exists')
+      }
+
+      // Create new repo
+      const timestamp = Date.now()
+
+      await createRepoDocument(appState)(repoId, {
+        timestamp
+      })
+
+      // Response:
+
+      const responseData: PutStoreResponse = undefined
+      res.status(201).json(responseData)
     }
-
-    const { syncKey } = params
-    const repoId = syncKeyToRepoId(syncKey)
-
-    if (await checkRepoExists(appState)(repoId)) {
-      throw new ServerError(409, 'Datastore already exists')
-    }
-
-    // Create new repo
-    const timestamp = Date.now()
-
-    await createRepoDocument(appState)(repoId, {
-      timestamp
-    })
-
-    // Response:
-
-    const responseData: PutStoreResponse = undefined
-    res.status(201).json(responseData)
-  })
+  )
 
   return router
 }
